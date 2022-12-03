ceb4ec4367d3a832eb1d16970f9110b1eb24a440
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2022-12-03 12:56:09 -0500

    Add files via upload

---
+++ setup.bash
@@ -0,0 +1,2 @@
+# Copyright (c) 2022, Aarik Pokras. All rights reserved.
+clear;python3 fuzz.py
---
+++ start.py
@@ -0,0 +1,7 @@
+from version import ver
+# vers=version.ver
+print("Welcome to BumfuzzleOS! This is version "+ver+" command line.\n🔔 This is the Alpha version, so it kinda sucks.")
+name=input('Enter desired hostname. ==> ')
+
+
+# Make continous imports of py files to make full command line file - import variables with import <filename>, then to import variables, <new-var-name>=<filename>.<old-var-name>
