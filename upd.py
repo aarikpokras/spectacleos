ddad6e5259946be2849df8468edb48836f2b4474
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2022-12-08 09:06:17 -0500

    Update upd.py

--- upd.py
+++ upd.py
@@ -1,7 +1,7 @@
 import requests
 updver=requests.get(url = 'https://ninth-destiny-mare.glitch.me/specsver').text
 
-if updver == "3.0.1":
+if updver == "3.2.0":
     print("██ spectacleOS is up to date. ██")
 else:
     print("██ Not up to date. Please update at https://github.com/aarikpokras/spectacleos. ██")
