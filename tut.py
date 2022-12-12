7de5e55930b2658529306fc99ba3ed495d75db38
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2022-12-11 19:04:07 -0500

    Delete tut.py

--- tut.py
+++
@@ -1,3 +0,0 @@
-input("Welcome to spectacleOS! Here is a short tutorial explaining how to use it. Press enter to continue.")
-input("To run a command, type it in and hit enter. Maybe try a command like `help`! Press enter to continue.")
-input("Now you're ready for a great spectacle adventure! After this, once you're back in the bash shell, run `./specs` to run spectacleOS! Press enter to exit.")
