fe568be79523f9d63fdf7e93d030b396b16cf89e
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2022-12-17 19:14:12 +0000

    Pending changes exported from your codespace

---
+++ upd.py
@@ -0,0 +1,8 @@
+import requests
+updver=requests.get(url = 'https://ninth-destiny-mare.glitch.me/specsver').text
+
+if updver == "3.2.0":
+    print("██ spectacleOS is up to date. ██")
+else:
+    print("██ Not up to date. Please update at https://github.com/aarikpokras/spectacleos. ██")
+    
---
+++ version.py
@@ -0,0 +1,2 @@
+# insert version here and import var from here to start.py
+ver="Fuji v3.2.0"
