ef9f7303222139a9a1d7db27ae8c402541da8408
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-01-19 11:03:46 -0500

    Add additional instructions

--- tutorial/tut.cpp
+++ tutorial/tut.cpp
@@ -4,10 +4,10 @@ int main () {
     string a;
     string b;
     string c;
-    cout << "Welcome to spectacleOS! Here is a short tutorial explaining how to use it. Press enter to continue.";
+    cout << "Welcome to spectacleOS! Here is a short tutorial explaining how to use it. Type a character and hit enter to continue.";
     cin >> a;
-    cout << "To run a command, type it in and hit enter. Maybe try a command like `help`! Press enter to continue.";
+    cout << "To run a command, type it in and hit enter. Maybe try a command like `help`!";
     cin >> b;
-    cout << "Now you're ready for a great spectacle adventure! After this, once you're back in the bash shell, run `./specs` to run spectacleOS! Press enter to exit.";
+    cout << "Now you're ready for a great spectacle adventure! After this, once you're back in the bash shell, run `./specs` to run spectacleOS! Type a character and hit enter to exit.";
     cin >> c;
 }
