dcd2cbf49921da0a57209cbb93a3a755a3ea4f38
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-01-19 10:55:32 -0500

    Change to ruby

---
+++ upd.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+###############
+fver='5.5.2'
+###############
+def openurl(url)
+  Net::HTTP.get(URI.parse(url))
+end
+
+puts "Current version: " + fver.chomp
+
+ver = openurl('http://ninth-destiny-mare.glitch.me/specsver')
+if ver != fver
+puts "██ Not up to date. Please update at https://github.com/aarikpokras/spectacleos. ██"
+else
+puts "██ spectacleOS is up to date. ██"
+end
