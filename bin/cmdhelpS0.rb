f1d1b18570f09773bcc51ab7eefc654646eea707
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-01-19 19:00:33 -0500

    Add tutorial to help

--- bin/cmdhelpS0.rb
+++ bin/cmdhelpS0.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-print "\033[1;31m[ -h ]\033[0m			help\n\033[1;31m[ start ]\033[0m		starts spectacleOS\n\033[1;31m[ --updates ]\033[0m		Check for updates\n"
+print "\033[1;31m[ -h ]\033[0m			help\n\033[1;31m[ start ]\033[0m		starts spectacleOS\n\033[1;31m[ --updates ]\033[0m		Check for updates\n[ --tutorial ]\033[0m		Tutorial"
