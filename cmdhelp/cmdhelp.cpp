3aad6d7b56d5fd4c56fb1d1267a7d3f4c8da4c87
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-01-21 09:23:44 -0500

    Update cmdhelp.cpp

--- cmdhelp/cmdhelp.cpp
+++ cmdhelp/cmdhelp.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 int main () {
-    cout << "\033[1;31m[ -h ]\033[0m			help\n\033[1;31m[ start ]\033[0m		starts spectacleOS\n\033[1;31m[ --updates ]\033[0m		Check for updates\n\033[1;31m[ --tutorial ]		Tutorial\n";
+    cout << "\033[1;31m[ -h ]\033[0m			help\n\033[1;31m[ start ]\033[0m		starts spectacleOS\n\033[1;31m[ --updates ]\033[0m		Check for updates\n\033[1;31m[ --tutorial ]\033[0m		Tutorial\n";
 }
