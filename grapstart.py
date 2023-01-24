71115cc25cddf2412e27470a39c9419038c83b4d
Aarik Pokras <aarik@searchme.one>
2023-01-24 11:53:36 -0500

    Added a graphical greeting screen for `.\/specs start`

---
+++ grapstart.py
@@ -0,0 +1,18 @@
+import turtle
+import time
+turtle.fillcolor("black")
+turtle.begin_fill()
+turtle.hideturtle()
+turtle.write("spectacleOS", False, align="center", font=('Mukta', 20, 'normal'))
+turtle.pen(fillcolor="black", pencolor="white", pensize=0)
+turtle.goto(0,-20)
+turtle.pen(fillcolor="black", pencolor="black", pensize=0)
+turtle.write("matterhorn", False, align="center", font=('Mukta', 10, 'normal'))
+turtle.pen(fillcolor="black", pencolor="white", pensize=0)
+turtle.goto(0, -40)
+turtle.pen(fillcolor="black", pencolor="black", pensize=0)
+turtle.write("Hi there!", False, align="center", font=('Mukta', 10, 'normal'))
+turtle.end_fill()
+turtle.done()
+exit()
+import me
